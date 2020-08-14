8a74facaaa20f46ed87938287725a44fa6c66642
siddhama <siddharth2001dhama@gmail.com>
2020-08-14 17:50:12 +0000

    updated background

---
+++ js/bgswitch.js
@@ -0,0 +1,25 @@
+var images = [];
+        if (window.matchMedia("(min-width: 492px)").matches) {
+  images=['images/bg/mtb4.jpg',
+  'images/bg/2-1.jpg',
+  'images/bg/5-1.jpg',
+  'images/bg/8-1.jpg'];
+} else {
+  images=['images/bg/mtb4_s.jpg',
+  'images/bg/2-1_s.jpg',
+  'images/bg/5-1_s.jpg',
+  'images/bg/8-1_s.jpg'];
+}
+$(document).ready(function(){
+  $(".header").onload(function(){
+    $("header").fadeOut(1000);
+  });
+  $(".header").onload(function(){
+    $("header").fadeIn(1000);
+  });
+});
+var slider = setInterval(function() {
+  document.getElementsByClassName('header')[0].setAttribute('style', 'background-image: url("'+images[0]+'")');
+  images.splice(images.length, 0, images[0]);
+  images.splice(0, 1);
+}, 2000);
