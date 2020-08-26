c10137940d2152a9a55ca0332cfd98ae2d77c345
siddhama <siddharth2001dhama@gmail.com>
2020-08-26 18:25:23 +0000

    service worker added

--- sw-register.js
+++ sw-register.js
@@ -1,10 +1,11 @@
-if('serviceWorker' in navigator){
-    window.addEventListener('load',()=>{
-        navigator.serviceWorker.register('/sw.js',{
-            scope:'/'
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load',  () => {
+        navigator.serviceWorker.register('/sw.js', {
+            scope: '/'
         })
-        .then(registration=>{
-            console.log("Service worker registration completed.");
+        .then(registration => {
+            console.log("Service Worker registration completed ...");
         });
     });
-}
+} 
